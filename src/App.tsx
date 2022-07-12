01e0a1822e86a9da15ab8fad6c4ad1f775fdcdf1
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-07-12 11:38:22 -0300

    configuração router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>ola mundo!</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
 
