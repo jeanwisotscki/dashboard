4f5d82dc632703ad202274deead610d5ff5992b6
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-07-22 14:55:28 -0300

    handle delete

--- src/pages/DetalhePessoa/index.tsx
+++ src/pages/DetalhePessoa/index.tsx
@@ -1,37 +1,75 @@
+import { LinearProgress } from "@mui/material";
+import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DetailsToolbar } from "../../shared/components/DetailsToolbar";
 import { LayoutBasePage } from "../../shared/layouts/LayoutBasePage";
+import { pessoasServices } from "../../shared/services/api/pessoas/pessoasServices";
 
 export const DetalhePessoa: React.FC = () => {
   const { id = "nova" } = useParams<"id">();
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [name, setName] = React.useState("");
+
   const handleSave = () => {
     console.log("salvar");
   };
 
-  const handleDelete = () => {
-    console.log("delete");
+  const handleDelete = (id: number) => {
+    if (confirm("Realmente deseja apagar o registro?")) {
+      pessoasServices.deleteById(id).then((res) => {
+        if (res instanceof Error) {
+          alert(res.message);
+          return;
+        }
+
+        alert("Registro apagado com sucesso!");
+        navigate("/pessoas");
+      });
+    }
   };
+
+  React.useEffect(() => {
+    if (id !== "nova") {
+      setIsLoading(true);
+
+      pessoasServices.getById(Number(id)).then((res) => {
+        setIsLoading(false);
+
+        if (res instanceof Error) {
+          alert(res.message);
+          navigate("/pessoas");
+          return;
+        }
+
+        setName(res.nomeCompleto);
+      });
+    }
+  }, [id]);
 
   return (
     <LayoutBasePage
-      titulo="Adicionar registro"
+      titulo={id === "nova" ? "Adicionar registro" : name.toUpperCase()}
       toolbar={
         <DetailsToolbar
           textAddButton="Nova"
           showSaveAndBackButton
           showAddButton={id !== "nova"}
           showDeleteButton={id !== "nova"}
-          onClickInAdd={() => navigate("/pessoas/detalhes/nova")}
-          onClickInBack={() => navigate("/pessoas/detalhes")}
-          onClickInSave={() => handleSave}
-          onClickInDelete={() => handleDelete}
-          onClickInSaveAndBack={() => handleSave}
+          onClickInAdd={() => navigate("/pessoas/detalhe/nova")}
+          onClickInBack={() => navigate("/pessoas")}
+          onClickInSave={() => handleSave()}
+          onClickInDelete={() => handleDelete(Number(id))}
+          onClickInSaveAndBack={() => handleSave()}
         />
       }
     >
-      <h1>detalhe pessoa {id}</h1>
+      {isLoading && <LinearProgress variant="indeterminate" />}
+      <div>
+        <h1>detalhe pessoa</h1>
+        <p>{id}</p>
+      </div>
     </LayoutBasePage>
   );
 };
